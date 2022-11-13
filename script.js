c6988f70affc10a9d92534a4fabf454171758d4e
Billy <Lrdweaver@gmail.com>
2022-11-13 16:55:34 -0500

    '.' limitations

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let numberSelection = document.querySelectorAll('.number');
 let clearSelection = document.querySelector('#clearBtn');
 let equals = document.querySelector('#equalsBtn');
 
+var screen = 0
 var num1 = 0
 var num2 = 0
 var arithmetic = ''
@@ -12,13 +13,13 @@ clearSelection.addEventListener('click', () => {
     num1 = 0
     arithmetic = ''
     display.innerHTML = ''
-    console.log(num1)
-    console.log(arithmetic)
 })
 
 numberSelection.forEach(btn => {
     btn.addEventListener('click', () => {
-        display.innerHTML += btn.textContent;
+        if (display.innerHTML.includes('.') && btn.innerHTML === '.'){
+            return
+        } screen = display.innerHTML += btn.textContent;
     });
 })
 
@@ -26,30 +27,27 @@ arithmeticSelection.forEach(btn =>{
     btn.addEventListener('click', () => {
         arithmetic = btn.innerHTML
         if (num1 == 0) {
-            num1 = display.innerHTML
+            num1 = Number(display.innerHTML)
+            console.log(num1)
             display.innerHTML = ''
-            console.log(arithmetic);
-            console.log(num1);
         } else {
-            console.log(arithmetic);
-            console.log(num1);
             return;
         }
     });
 });
 
 equals.addEventListener('click', () => {
+    let screen = Number(display.innerHTML);
+    console.log(num1)
     if (arithmetic === 'x') {
-        console.log('multiplication')
-        display.innerHTML = (num1 * display.innerHTML);
+        display.innerHTML = (num1 * screen);
     } else if (arithmetic === '+') {
-        console.log('addition')
-        display.innerHTML = (num1 + display.innerHTML);
+        display.innerHTML = (num1 + screen);
     } else if (arithmetic === '-') {
-        console.log('subtraction')
-        display.innerHTML = (num1 - display.innerHTML);
+        display.innerHTML = (num1 - screen);
     } else if (arithmetic === '/') {
-        console.log('divison')
-        display.innerHTML = (num1 / display.innerHTML);
+        display.innerHTML = (num1 / screen);
+    } else if (arithmetic === '%') {
+        display.innerHTML = (num1 % screen);
     }
 });
