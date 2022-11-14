17ce1e814b66948505bc9b0f61c81f922c8d5788
Billy <Lrdweaver@gmail.com>
2022-11-14 08:35:28 -0500

    Works but uses eval function

--- script.js
+++ script.js
@@ -5,12 +5,52 @@ let clearSelection = document.querySelector('#clearBtn');
 let deleteLast = document.querySelector('.delete')
 let equals = document.querySelector('#equalsBtn');
 
-var screen = 0
 var num1 = 0
 var num2 = 0
+var num3 = 0
 var arithmetic = ''
+display.innerHTML = ''
+
+deleteLast.addEventListener('click', () => {
+    let newArray = display.innerHTML
+    .split('')
+    .slice(0, -1)
+    .join('')
+    display.innerHTML = newArray
+})
 
 clearSelection.addEventListener('click', () => {
+    display.innerHTML = ''
+})
+numberSelection.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (display.innerHTML.includes('.') && btn.innerHTML === '.'){
+            return;
+        }; display.innerHTML += `${btn.innerHTML}`
+    })
+})
+
+arithmeticSelection.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (display.innerHTML == 0) {
+            display.innerHTML = ''
+            return;
+        }
+        display.innerHTML += `${btn.innerHTML}`
+    })
+})
+
+equals.addEventListener('click', () => {
+    let evaluate = eval(display.innerHTML)
+    display.innerHTML = evaluate
+})
+
+
+
+
+
+
+/* clearSelection.addEventListener('click', () => {
     num1 = 0
     arithmetic = ''
     display.innerHTML = ''
@@ -24,36 +64,61 @@ deleteLast.addEventListener('click', () => {
 numberSelection.forEach(btn => {
     btn.addEventListener('click', () => {
         if (display.innerHTML.includes('.') && btn.innerHTML === '.'){
-            return
-        } screen = display.innerHTML += btn.textContent;
-    });
-})
-
-arithmeticSelection.forEach(btn =>{
-    btn.addEventListener('click', () => {
-        arithmetic = btn.innerHTML
-        if (num1 == 0) {
-            num1 = Number(display.innerHTML)
-            console.log(num1)
-            display.innerHTML = ''
-        } else {
             return;
+        };
+        if (num1 === 0) {
+            display.innerHTML += btn.innerText
+            num1 = display.innerHTML
+        } else if (num1 !== 0) {
+            display.innerHTML += btn.innerHTML
+            num2 = display.innerHTML
         }
     });
 });
 
-equals.addEventListener('click', () => {
-    let screen = Number(display.innerHTML);
-    console.log(num1)
-    if (arithmetic === 'x') {
-        display.innerHTML = (num1 * screen);
-    } else if (arithmetic === '+') {
-        display.innerHTML = (num1 + screen);
-    } else if (arithmetic === '-') {
-        display.innerHTML = (num1 - screen);
-    } else if (arithmetic === '/') {
-        display.innerHTML = (num1 / screen);
-    } else if (arithmetic === '%') {
-        display.innerHTML = (num1 % screen);
-    }
+arithmeticSelection.forEach(btn =>{
+    btn.addEventListener('click', () => {
+        arithmetic = btn.innerHTML;
+        display.innerHTML = ''
+        if (num2 !== 0) {
+            equalsFunction();
+        }
+        console.log(`arithmetic is ${arithmetic}`)
+    });
 });
+
+equals.addEventListener('click', () => {
+    equalsFunction()
+});
+
+function equalsFunction() {
+    num2 = Number(display.innerHTML);
+    if (arithmetic === 'x') {
+        display.innerHTML = (num1 * num2);
+        num3 = (num1 * num2);
+    } else if (arithmetic === '+') {
+        num3 = num1 + num2;
+        display.innerHTML = num3;
+        console.log(`equals num1: ${num1}`)
+        console.log(`equals num2: ${num2}`)
+        console.log(`equals num3: ${num3}`)
+    } else if (arithmetic === '-') {
+        num3 = num1 - num2;
+        display.innerHTML = num3;
+        console.log(`equals num1: ${num1}`)
+        console.log(`equals num2: ${num2}`)
+        console.log(`equals num3: ${num3}`)
+    } else if (arithmetic === '/') {
+        num3 = num1 / num2;
+        display.innerHTML = num3;
+        console.log(`equals num1: ${num1}`)
+        console.log(`equals num2: ${num2}`)
+        console.log(`equals num3: ${num3}`)
+    } else if (arithmetic === '%') {
+        num3 = num1 % num2;
+        display.innerHTML = num3;
+        console.log(`equals num1: ${num1}`)
+        console.log(`equals num2: ${num2}`)
+        console.log(`equals num3: ${num3}`)
+    };
+}; */
