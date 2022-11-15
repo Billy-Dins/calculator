0a99d9008a4949b1ff723efa3306dabba5ea628f
Billy <Lrdweaver@gmail.com>
2022-11-14 19:12:33 -0500

    ready for merge

--- script.js
+++ script.js
@@ -8,66 +8,62 @@ let equals = document.querySelector('#equalsBtn');
 var screen = 0
 var num1 = 0
 var num2 = 0
-var num3 = 0
 var operator = ''
 
+/* 
+# To Do:
+-Add selector value for operators
+-Add character limits in the display 
+*/
 
+deleteLast.addEventListener('click', () => {
+    let displayArray = display.innerHTML.split('').slice(0, -1).join('');
+    display.innerHTML = displayArray
+});
 clearSelection.addEventListener('click', () => {
-        num1 = 0;
-        num2 = 0;
-        num3 = 0;
-        operator = '';
-        display.textContent = 0;
-    });
+    num1 = '';
+    num2 = 0;
+    operator = '';
+    display.textContent = 0;
+});
 
 equals.addEventListener('click', () => {
     if (num2 !== 0) {
-        console.log(num1)
-        console.log(num2)
-        console.log(operator)
     num2 = solve(num2,num1,operator)
     num1 = 0
     }else if (num2 === 0) {
         num2 = display.textContent
-        console.log('error at equals')
     }
 });
 
-
 numberSelection.forEach(btn => {
     btn.addEventListener('click', () => {
+        if (display.innerHTML.includes('.') && btn.innerHTML === '.'){
+            return
+        }
         if (num1 !== 0 && operator !=='' && num2 === 0) {
-            console.log('1')
             display.textContent = '';
             num2 = btn.textContent;
             display.textContent = num2
         } else if (num1 == 0 && num2 !== 0){
-            console.log('2')
             display.textContent = ''
             num1 = btn.textContent
             display.textContent = num1
         } else if (num1 !== 0 && num2 !== 0) {
-            console.log('3')
             display.textContent = ''
             num1 += btn.textContent;
             display.textContent = num1
         } else if (num1 === 0 && num2 === 0) {
-            console.log('4')
             display.textContent += btn.textContent
-            console.log(`num1 is ${num1}`);
         } else {
-            console.log('5')
             num1 += btn.textContent;
             display.textContent = num1;
-            console.log(`num1 is ${num1}`);
         }
     });
 });
 
-
 arithmeticSelection.forEach(btn => {
     btn.addEventListener('click', () => {
-        console.log(`operator is ${operator}`)
         if (num2 === 0 && num1 !== 0) {
             operator = btn.textContent;
             num2 = num1
@@ -81,8 +77,8 @@ arithmeticSelection.forEach(btn => {
         num2 = display.textContent
         num1 = 0
         }
-    })
-})
+    });
+});
 
 function solve(num1, num2) {
     num1 = Number(num1);
